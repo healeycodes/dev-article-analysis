98e4b38b1c9a0e3a005b18e60c38874dd73da664
Andrew <healeycodes@gmail.com>
2020-01-20 19:34:43 +0000

    Update example data

--- exampleData.js
+++ exampleData.js
@@ -1,203 +1,202 @@
 module.exports = [
   {
     title: "Benchmarking WebSocket Servers with Python!",
-    readability: { fleschReadingEase: 24.92, fleschKincaidGrade: 17 },
-    sentiment: 0.015570934256055362
+    readability: { fleschReadingEase: 45.59, fleschKincaidGrade: 13.2 },
+    sentiment: 0.027573529411764705
   },
   {
-    title:
-      "Cloning Dominos Order Tracker as a Microservice with Node.js and Firebase!",
-    readability: { fleschReadingEase: -41.58, fleschKincaidGrade: 36.4 },
-    sentiment: 0.0015267175572519084
+    title: "Cloning Dominos Order Tracker as a Microservice with Node.js and Firebase!",
+    readability: { fleschReadingEase: 43.19, fleschKincaidGrade: 16.2 },
+    sentiment: 0.06043956043956044
   },
   {
     title: "I Ported Sudoku to Your URL Bar and It Plays like a Dream",
-    readability: { fleschReadingEase: 14.49, fleschKincaidGrade: 16.9 },
-    sentiment: 0.07954545454545454
+    readability: { fleschReadingEase: 50.67, fleschKincaidGrade: 11.3 },
+    sentiment: 0.08695652173913043
   },
   {
-    title:
-      "Real-time streaming of every emoji posted as a comment to Reddit.com 🔥",
-    readability: { fleschReadingEase: 16.8, fleschKincaidGrade: 20.2 },
-    sentiment: 0.019337016574585635
+    title: "Real-time streaming of every emoji posted as a comment to Reddit.com 🔥",
+    readability: { fleschReadingEase: 60.18, fleschKincaidGrade: 11.8 },
+    sentiment: 0.016771488469601678
   },
   {
     title: "Creating Cute GitHub Badges Based Off a Project's Mood (Node.js)",
-    readability: { fleschReadingEase: 26.34, fleschKincaidGrade: 16.5 },
-    sentiment: 0.03081232492997199
+    readability: { fleschReadingEase: 50.36, fleschKincaidGrade: 11.4 },
+    sentiment: 0.03359683794466403
   },
   {
-    title:
-      "Two Things About My First In-house Software Job That I Didn't See Coming",
-    readability: { fleschReadingEase: 52.63, fleschKincaidGrade: 12.6 },
-    sentiment: 0.042316258351893093
+    title: "Two Things About My First In-house Software Job That I Didn't See Coming",
+    readability: { fleschReadingEase: 62.82, fleschKincaidGrade: 10.8 },
+    sentiment: 0.057736720554272515
   },
   {
     title: "Kicking Butt and Taking Names With WebSockets (Tutorial)",
-    readability: { fleschReadingEase: 38.76, fleschKincaidGrade: 13.8 },
-    sentiment: 0.030439684329199548
+    readability: { fleschReadingEase: 60.55, fleschKincaidGrade: 9.6 },
+    sentiment: 0.03614457831325301
   },
   {
     title: "A Day in the Life of a Mediumly Productive Software Engineer",
-    readability: { fleschReadingEase: 67.89, fleschKincaidGrade: 8.8 },
-    sentiment: 0.034482758620689655
+    readability: { fleschReadingEase: 68.1, fleschKincaidGrade: 8.7 },
+    sentiment: 0.03521703521703522
   },
   {
     title: " How Flappy Bird Got Me My Start in Software",
-    readability: { fleschReadingEase: 61.26, fleschKincaidGrade: 9.3 },
-    sentiment: 0.040880503144654086
+    readability: { fleschReadingEase: 61.67, fleschKincaidGrade: 9.1 },
+    sentiment: 0.04424778761061947
   },
   {
     title: "Coding a Live Cam for the Raspberry Pi in Python (Tutorial)",
-    readability: { fleschReadingEase: 35.41, fleschKincaidGrade: 15.1 },
-    sentiment: 0.023336214347450302
+    readability: { fleschReadingEase: 60.35, fleschKincaidGrade: 9.6 },
+    sentiment: 0.015363128491620111
   },
   {
-    title:
-      "Earn a Build Passing Badge on GitHub ✅! Testing Your Express App with Travis CI",
-    readability: { fleschReadingEase: 49.69, fleschKincaidGrade: 13.7 },
-    sentiment: 0.011086474501108648
+    title: "Earn a Build Passing Badge on GitHub ✅! Testing Your Express App with Travis CI",
+    readability: { fleschReadingEase: 66.37, fleschKincaidGrade: 9.4 },
+    sentiment: 0.01615508885298869
   },
   {
-    title:
-      "Need a Friend? Write a Reddit Bot with Python to Follow You Around 🤖",
-    readability: { fleschReadingEase: 24.82, fleschKincaidGrade: 17.1 },
-    sentiment: 0.03225806451612903
+    title: "Need a Friend? Write a Reddit Bot with Python to Follow You Around 🤖",
+    readability: { fleschReadingEase: 57.3, fleschKincaidGrade: 10.8 },
+    sentiment: 0.0411663807890223
   },
   {
     title: "JavaScript One-Liners That Make Me Excited",
-    readability: { fleschReadingEase: 37.74, fleschKincaidGrade: 14.2 },
-    sentiment: 0.047021943573667714
+    readability: { fleschReadingEase: 60.04, fleschKincaidGrade: 9.8 },
+    sentiment: 0.08163265306122448
   },
   {
     title: "Answered: What the Heck is Code Golf? ⛳",
-    readability: { fleschReadingEase: 46.1, fleschKincaidGrade: 13 },
-    sentiment: 0.056100981767180924
+    readability: { fleschReadingEase: 51.58, fleschKincaidGrade: 10.9 },
+    sentiment: 0.06802721088435375
   },
   {
     title: "Build a Python Bot to Find Your Website's Dead Links",
-    readability: { fleschReadingEase: 42.34, fleschKincaidGrade: 14.5 },
-    sentiment: -0.011764705882352941
+    readability: { fleschReadingEase: 69.62, fleschKincaidGrade: 8.1 },
+    sentiment: -0.0032258064516129032
   },
   {
     title: "Embed Your Latest DEV Posts Anywhere 💌",
-    readability: { fleschReadingEase: 34.29, fleschKincaidGrade: 15.5 },
-    sentiment: 0.022972972972972974
+    readability: { fleschReadingEase: 68.6, fleschKincaidGrade: 8.5 },
+    sentiment: 0.03953488372093023
   },
   {
     title: "Learn the Storage API by Building a Note Taking App",
-    readability: { fleschReadingEase: 33.78, fleschKincaidGrade: 15.7 },
-    sentiment: 0.015277777777777777
+    readability: { fleschReadingEase: 68.4, fleschKincaidGrade: 8.6 },
+    sentiment: 0.021953896816684963
   },
   {
     title: "Talking to Python from JavaScript (and Back Again!)",
-    readability: { fleschReadingEase: 42.85, fleschKincaidGrade: 14.3 },
-    sentiment: 0.024918032786885248
+    readability: { fleschReadingEase: 59.53, fleschKincaidGrade: 9.9 },
+    sentiment: 0.026530612244897958
   },
   {
     title: "How to Write an Awesome GitHub README",
-    readability: { fleschReadingEase: 57.4, fleschKincaidGrade: 10.8 },
-    sentiment: 0.08478605388272584
+    readability: { fleschReadingEase: 58.01, fleschKincaidGrade: 10.5 },
+    sentiment: 0.08763308763308764
   },
   {
     title: "The Weird Rules I Set Myself That Got Me a Job",
-    readability: { fleschReadingEase: 59.43, fleschKincaidGrade: 10 },
-    sentiment: 0.048329779673063254
+    readability: { fleschReadingEase: 59.53, fleschKincaidGrade: 9.9 },
+    sentiment: 0.046528274874731566
   },
   {
     title: "What They Don't Teach You at a Computer Science Masters",
-    readability: { fleschReadingEase: 61.06, fleschKincaidGrade: 9.4 },
-    sentiment: 0.06956521739130435
+    readability: { fleschReadingEase: 61.16, fleschKincaidGrade: 9.3 },
+    sentiment: 0.0697928026172301
   },
   {
     title: "Fun With Linear Time: My Favorite Algorithm",
-    readability: { fleschReadingEase: 38.18, fleschKincaidGrade: 16.1 },
-    sentiment: 0.044964028776978415
+    readability: { fleschReadingEase: 45.69, fleschKincaidGrade: 13.2 },
+    sentiment: 0.05463182897862233
   },
   {
     title: "Continuous Deployment to Glitch via GitHub Webhooks",
-    readability: { fleschReadingEase: 37.03, fleschKincaidGrade: 14.5 },
-    sentiment: 0.019402985074626865
+    readability: { fleschReadingEase: 58.52, fleschKincaidGrade: 10.3 },
+    sentiment: 0.025341130604288498
   },
   {
     title: "A Tiny Project, From Inception to Deployment",
-    readability: { fleschReadingEase: 20.08, fleschKincaidGrade: 21 },
-    sentiment: 0.008849557522123894
+    readability: { fleschReadingEase: 60.69, fleschKincaidGrade: 11.6 },
+    sentiment: 0.018281535648994516
   },
   {
     title: "Solving Puzzles With High-Performance JavaScript",
-    readability: { fleschReadingEase: 62.11, fleschKincaidGrade: 11 },
-    sentiment: 0.029900332225913623
+    readability: { fleschReadingEase: 62.68, fleschKincaidGrade: 8.7 },
+    sentiment: 0.016243654822335026
   },
   {
     title: "Interview Question: Implement a Progress Bar",
-    readability: { fleschReadingEase: 57.61, fleschKincaidGrade: 10.7 },
-    sentiment: 0.06666666666666667
+    readability: { fleschReadingEase: 69.92, fleschKincaidGrade: 8 },
+    sentiment: 0.0748663101604278
   },
   {
     title: "Saving Data in JavaScript Without a Database",
-    readability: { fleschReadingEase: 20.18, fleschKincaidGrade: 20.9 },
-    sentiment: 0.041501976284584984
+    readability: { fleschReadingEase: 44.61, fleschKincaidGrade: 15.7 },
+    sentiment: 0.04247104247104247
   },
   {
-    title:
-      "Refactoring JavaScript for Performance and Readability (with Examples!)",
-    readability: { fleschReadingEase: 22.11, fleschKincaidGrade: 20.2 },
-    sentiment: 0.022222222222222223
+    title: "Refactoring JavaScript for Performance and Readability (with Examples!)",
+    readability: { fleschReadingEase: 56.69, fleschKincaidGrade: 11 },
+    sentiment: 0.03193033381712627
   },
   {
     title: "Your First Open-Source Pull Request: a Walkthrough",
-    readability: { fleschReadingEase: 60.14, fleschKincaidGrade: 9.7 },
-    sentiment: 0.023157894736842106
+    readability: { fleschReadingEase: 60.35, fleschKincaidGrade: 9.6 },
+    sentiment: 0.024494142705005325
   },
   {
     title: "A Tutorial on Tricking Google: Encoding Messages in Apostrophes",
-    readability: { fleschReadingEase: 36.15, fleschKincaidGrade: 16.9 },
-    sentiment: 0.016483516483516484
+    readability: { fleschReadingEase: 55.47, fleschKincaidGrade: 11.5 },
+    sentiment: 0.02875399361022364
   },
   {
     title: "What If I Want My Website to Last for 100 Years?",
     readability: { fleschReadingEase: 48.94, fleschKincaidGrade: 11.9 },
-    sentiment: 0.040289256198347105
+    sentiment: 0.04033092037228542
   },
   {
     title: "An Introduction to Caching on the Web, With Examples in Python",
-    readability: { fleschReadingEase: 44.88, fleschKincaidGrade: 13.5 },
-    sentiment: 0.03948576675849403
+    readability: { fleschReadingEase: 57.5, fleschKincaidGrade: 10.7 },
+    sentiment: 0.04503464203233257
   },
   {
     title: "My Experience with Pair Programming",
-    readability: { fleschReadingEase: 54.86, fleschKincaidGrade: 11.7 },
-    sentiment: 0.00902061855670103
+    readability: { fleschReadingEase: 54.97, fleschKincaidGrade: 11.7 },
+    sentiment: 0.009079118028534372
   },
   {
     title: "I Built a Bot to Try and Get Money Back From My Internet Provider",
-    readability: { fleschReadingEase: 25.87, fleschKincaidGrade: 18.7 },
-    sentiment: 0.04264870931537598
+    readability: { fleschReadingEase: 50.91, fleschKincaidGrade: 13.3 },
+    sentiment: 0.040880503144654086
   },
   {
     title: "My First Golang Program",
-    readability: { fleschReadingEase: 59.03, fleschKincaidGrade: 10.1 },
-    sentiment: 0.059726962457337884
+    readability: { fleschReadingEase: 59.74, fleschKincaidGrade: 9.9 },
+    sentiment: 0.06952491309385864
   },
   {
     title: "Cloning Memcached with Go",
-    readability: { fleschReadingEase: 36.25, fleschKincaidGrade: 16.8 },
-    sentiment: 0.014375561545372867
+    readability: { fleschReadingEase: 51.31, fleschKincaidGrade: 13.1 },
+    sentiment: 0.020656136087484813
   },
   {
     title: "Let's Build a CAPTCHA Generator with Node.js",
-    readability: { fleschReadingEase: 30.16, fleschKincaidGrade: 19.2 },
-    sentiment: 0.012829650748396294
+    readability: { fleschReadingEase: 55.61, fleschKincaidGrade: 13.5 },
+    sentiment: 0.019845644983461964
   },
   {
     title: "Hardest JavaScript Puzzle I've Ever Solved",
-    readability: { fleschReadingEase: 34.16, fleschKincaidGrade: 19.7 },
-    sentiment: 0.007368421052631579
+    readability: { fleschReadingEase: 47.56, fleschKincaidGrade: 14.6 },
+    sentiment: 0.011315417256011316
   },
   {
     title: "Gatsby Sparks Joy",
-    readability: { fleschReadingEase: 43.66, fleschKincaidGrade: 14 },
-    sentiment: 0.05950095969289827
+    readability: { fleschReadingEase: 53.75, fleschKincaidGrade: 12.2 },
+    sentiment: 0.06224066390041494
+  },
+  {
+    title: "DEV Article Analysis",
+    readability: { fleschReadingEase: 46.91, fleschKincaidGrade: 12.7 },
+    sentiment: 0.04796511627906977
   }
 ];
